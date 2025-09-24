f772d364bbcd7be62a3ed4960371b250fdfd580c
radecodes202 <radecodes202@gmail.com>
2025-09-24 17:25:26 +0800

    Refactor code structure for improved readability and maintainability

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Announcement
 
-# Register your models here.
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'content']
